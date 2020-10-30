c47644300facf5e6cf018d2ffaf9baaad1c197cb
Adriel-Cochito <adriel.cochito@al.infnet.edu.br>
2020-10-30 06:58:26 -0300

    Criacao do projeto

---
+++ dispositivos.js
@@ -0,0 +1,203 @@
+var iframeProtecao = new Array()
+var iframeBotoeiras = new Array()
+var iframeDiversos = new Array()
+
+iframeProtecao[0] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd968b2ec00f4f9ca38f3?mode=embed"></iframe>' +
+    '</div>';
+iframeProtecao[1] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd96822bf798e652062d8?mode=embed"></iframe>' +
+    '</div>';
+iframeProtecao[2] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd968e27800cf181b547e?mode=embed"></iframe>' +
+    '</div>';
+iframeProtecao[3] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src=""https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd9686e09b38adb85f66d?mode=embed"></iframe>' +
+    '</div>';
+iframeProtecao[4] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd968162a02fb77ecb44c?mode=embed"></iframe>' +
+    '</div>';
+var descricaoProtecao = new Array()
+
+iframeBotoeiras[0] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd968f759dd6fe4629df8?mode=embed"></iframe>' +
+    '</div>';
+iframeBotoeiras[1] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd9688d5a03a372657fe3?mode=embed"></iframe>' +
+    '</div>';
+iframeBotoeiras[2] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd968f6faa2e29cc3bc77?mode=embed"></iframe>' +
+    '</div>';
+var descricaoBotoeiras = new Array()
+
+iframeDiversos[0] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd968db81250b2b72fe12?mode=embed"></iframe>' +
+    '</div>';
+iframeDiversos[1] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd9683ce2db965172dff8?mode=embed"></iframe>' +
+    '</div>';
+iframeDiversos[2] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd9688be970ed248de13a?mode=embed"></iframe>' +
+    '</div>';
+iframeDiversos[3] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd9685d36996dff0a1969?mode=embed"></iframe>' +
+    '</div>';
+iframeDiversos[4] = '<div id="iframe">' +
+    '<iframe title="A 3D model" width="100%" height="100%" src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd968bcbc15aa22a7f65e?mode=embed"></iframe>' +
+    '</div>';
+var descricaoDiversos = new Array()
+
+descricaoProtecao[0] = "É um dispositivo de proteção";
+descricaoProtecao[1] = "É um disjuntor com características de ajuste para motores";
+descricaoProtecao[2] = "É um borne de conexão que possui um fusível de proteção a linha de conexão";
+descricaoProtecao[3] = "É um tipo de fusível de proteção de altas correntes elétricas";
+descricaoProtecao[4] = "É um relé de proteção que vai acoplado a parte inferior de um contator, oferecendo proteções de sobrecorrente ao motor, com contatos auxiliares";
+
+descricaoBotoeiras[0] = "Botão de comando que retorna por mola a posição dos conattos ao desaciona-lo";
+descricaoBotoeiras[1] = "Possui posições que se mantem mecanicamente quando comutado";
+descricaoBotoeiras[2] = "Botoeira de soco com trava, para manter seu acionamento de segurança e emergência";
+
+descricaoDiversos[0] = "Controla um motor trifásico através do controle de frequência de saída";
+descricaoDiversos[1] = "É um motor de indução trifásico";
+descricaoDiversos[2] = "É um conector simples a parafusos";
+descricaoDiversos[3] = "Controla um motor através do controle da tensão de saída";
+descricaoDiversos[4] = "Controlador Lógico Programável, usado para criar controles automatizados eletrônicamente programáveis";
+
+
+const lista_protecao = [   
+    { id: 0, tipo: "Proteção", nome: 'Disjuntor', link: iframeProtecao[0], img: "disjuntores.jpg", lista: 0, descricao: descricaoProtecao[0]},
+    { id: 1, tipo: "Proteção", nome: 'Disjuntor Motor', link: iframeProtecao[1] , img: "disjuntor_motor.png", lista: 0, descricao: descricaoProtecao[1]},
+    { id: 2, tipo: "Proteção", nome: 'Borne Fusível', link: iframeProtecao[2], img: "borne_fusivel.png", lista: 0, descricao: descricaoProtecao[2]},
+    { id: 3, tipo: "Proteção", nome: 'Fusível NH', link: iframeProtecao[3], img: "fusivel_nh.png", lista: 0, descricao: descricaoProtecao[3]},
+    { id: 4, tipo: "Proteção", nome: 'Relé de sobrecarga', link: iframeProtecao[4], img: "rele_sobrecarga.jpg", lista: 0, descricao: descricaoProtecao[4]}
+];
+const lista_botoeira = [
+    { id: 0, tipo: "Botoeira", nome: 'Botão pulsador', link: iframeBotoeiras[0], img: "botao_pulsador.jpg", lista: 1, descricao: descricaoBotoeiras[0]},
+    { id: 1, tipo: "Botoeira", nome: 'Chave Seletora',link: iframeBotoeiras[1], img: "chave_seletora.jpg", lista: 1, descricao: descricaoBotoeiras[1]},
+    { id: 2, tipo: "Botoeira", nome: 'Botoeira de Emergência', link: iframeBotoeiras[2], img: "botoeira_emergencia.jpg", lista: 1, descricao: descricaoBotoeiras[2]}
+];
+const lista_diversa = [
+    { id: 0, tipo: "Inversor de Frequência", nome: 'CFW-10', link: iframeDiversos[0], img: "cfw_10.png", lista: 2, descricao: descricaoDiversos[0]},
+    { id: 1, tipo: "Trifásico", nome: 'Motor', link: iframeDiversos[1], img: "motor.png", lista: 2, descricao: descricaoDiversos[1]},
+    { id: 2, tipo: "Conexão", nome: 'Conector Sindal', link: iframeDiversos[2], img: "conector_sindal.jpg", lista: 2, descricao: descricaoDiversos[2]},
+    { id: 3, tipo: "Soft Starter", nome: 'SSW 05', link: iframeDiversos[3], img: "ssw_05.jpg", lista: 2, descricao: descricaoDiversos[3]},
+    { id: 4, tipo: "PLC", nome: 'Logo Siemens', link: iframeDiversos[4], img: "logo_siemens.png", lista: 2, descricao: descricaoDiversos[4]}
+];
+
+/* const lista_completa = []
+ */
+
+
+
+var protecao = new Array();
+var botoeiras = new Array();
+var diversos = new Array();
+var todos = new Array();
+var r1;
+var r2;
+
+
+function criar_html(lista, itens) {
+for (let i = 0; i < lista.length; i++) {
+    itens[i] = (' <div class="col mb-4">' +
+    '<div id="card_disp_und" class="card h-100"  onclick="ver_3d('+lista[i].lista+','+lista[i].id+')">' +
+    '<img src="img/componentes/'+lista[i].img +'" class="card-img-top" alt="...">' +
+    '<div class="card-body">' +
+    '<h5 class="card-title">' + lista[i].nome + '</h5> <hr>' +
+    '<p> <b>Tipo: </b> '+ lista[i].tipo +' </p> <hr>' +
+    '<p class="card-text"><b> Descrição: </b>' + lista[i].descricao + '</p>' +
+    '</div>' +
+    '</div>' +
+    '</div>'
+);
+} }
+
+criar_html(lista_botoeira, botoeiras);
+criar_html(lista_protecao, protecao);
+criar_html(lista_diversa, diversos);
+
+function fechar(lista) {
+    lista = ("<div class='row row-cols-1 row-cols-md-4'>" + lista.join('') + "</div");
+    return lista;
+}
+
+function juntarListas() {
+    for (let i = 0; i < botoeiras.length; i++) {
+        let x = todos.length
+        todos[x] = botoeiras[i];
+    }
+    for (let i = 0; i < protecao.length; i++) {
+        let x = todos.length
+        todos[x] = protecao[i];
+    }
+    for (let i = 0; i < diversos.length; i++) {
+        let x = todos.length
+        todos[x] = diversos[i];
+    }
+    todos = shuffle(todos)
+}
+
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+function onBtnClickedtodos() {
+    document.getElementById('r1').innerHTML = fechar(todos);
+};
+
+function onBtnClickedProtecao() {
+    document.getElementById('r1').innerHTML = fechar(protecao);
+};
+
+function onBtnClickedBotoeira() {
+    document.getElementById('r1').innerHTML = fechar(botoeiras);
+};
+function onBtnClickedDiversos() {
+    document.getElementById('r1').innerHTML = fechar(diversos);
+};
+
+juntarListas()
+document.getElementById('todos').onclick = onBtnClickedtodos;
+document.getElementById('protecao').onclick = onBtnClickedProtecao;
+document.getElementById('botoeiras').onclick = onBtnClickedBotoeira;
+document.getElementById('diversos').onclick = onBtnClickedDiversos;
+
+function pega_lista(tipo) {
+    if (tipo == 0) {
+        return lista_protecao;        
+    } else if(tipo == 1) {
+        return lista_botoeira;
+    } else if(tipo == 2) {
+        return lista_diversa;
+    };
+}
+
+function ver_3d(tipo, id) {
+    const lista = pega_lista(tipo)
+    console.log("Funcionou, id:" + id);
+   
+    var saida = '<div class="card mb-3" id="card-partidas">' +
+    '<div class="card-body">' +
+    '<h3 class="card-title">' + lista[id].nome + '</h3>' +
+    lista[id].link +
+    '<h3 class=""></h3>' +
+    '<h5 class="card-title">' + lista[id].nome + '</h5>'+
+    '<p> <b>Tipo: </b> '+ lista[id].tipo +' </p>' +
+    '<p class="card-text"><b> Descrição: </b>' + lista[id].descricao + '</p>' +
+    '</div></div></div>';
+    document.getElementById('r1').innerHTML = saida;
+    /*     document.getElementById('r1').innerHTML = '<div class="div-iframe" id="iframe"> <iframe src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd968531ce42f55be26fa?mode=embed"'+
+        'width="100%" height="100%" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"  frameborder="0"></iframe> </div';
+     */
+}
+
+window.onload = onBtnClickedtodos();
---
+++ partidas.js
@@ -0,0 +1,48 @@
+
+var iframePartida = new Array()
+iframePartida[0] = '<div class="card-img-top" > <iframe src="https://myhub.autodesk360.com/ue2c73da7/shares/public/SH56a43QTfd62c1cd9681b50363861215090?mode=embed"' +
+    'width="100%" height="500px" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"  frameborder="0"></iframe> </div';
+
+var descricaoPartida = new Array()
+descricaoPartida[0] = "É uma partida simples, com botões Liga e Desliga, além de outros componentes de proteção";
+
+const lista_partidas = [
+    { id: 1, nome: 'Partida direta', link: iframePartida[0], descricao: descricaoPartida[0] }
+/*     { id: 2, nome: 'Partida direta com reversão Intercalada', link: iframePartida[0], descricao: descricaoPartida[0] } */
+];
+
+var partidas = new Array();
+var menuTitulos = new Array();
+var r1;
+var r2;
+var saida;
+
+function onBtnClickedPartidas(id) {
+    /* class="card-img-top" */
+    saida = '<div class="card mb-3" id="card-partidas">' +
+        '<div class="card-body">' +
+        '<h3 class="card-title">' + lista_partidas[id-1].nome + '</h3>' +
+        '<p class="card-text"><b> Descrição: </b>' + lista_partidas[id-1].descricao + '</p>' + 
+        '<div class="diagrama"><h5> Diagrama:</h5> <img  src="img/diagramas/pd_dg.png" class="card-img" alt="..."></div>'+
+        /* '<object data="pdf/partida-direta.pdf" type="application/pdf">' +
+            '<p>Seu navegador não tem um plugin pra PDF</p>'+
+        '</object>'+ */
+        '<h5>Visualização 3D:</h5>' + lista_partidas[id - 1].link +
+        '<h3 class=""></h3>' +
+/*         '<h5 class="card-title">' + lista_partidas[id-1].nome + '</h5>'+
+        '<p class="card-text"><b> Descrição: </b>' + lista_partidas[id-1].descricao + '</p>' + */
+        '</div></div></div>'
+
+    document.getElementById('r1').innerHTML = saida;
+};
+
+for (let i = 0; i < lista_partidas.length; i++) {
+    menuTitulos[i] = '<a class="dropdown-item" href="#" onclick="onBtnClickedPartidas(' + lista_partidas[i].id + ')">' + lista_partidas[i].nome + '</a>'
+
+}
+
+document.getElementById('menu-partidas').innerHTML = '<div class="dropdown">' +
+    '<a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
+    'Escolha o tipo de partida </a>' +
+    '<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">' +
+    menuTitulos.join('') + '</div></div>'
